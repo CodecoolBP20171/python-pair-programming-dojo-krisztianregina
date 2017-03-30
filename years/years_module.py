9d4f44d602a82becd2a6e9ca13260d582d27dd70
ReginaCsaszar <regina.csaszar@gmail.com>
2017-03-30 09:41:25 +0200

    years done

--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,18 @@ import datetime
 
 
 def years(age):
-    return
+    now = datetime.datetime.now()
+    current = now.year
+    result = 99 - age + current
+    return result
 
 
 def main():
+    name = input("Your name?: ")
+    age = int(input("Your age: "))
+    turn = int(input("Give a number: "))
+    for i in range(turn): 
+        print(name, "\b, you will be 100 in", years(age))
     return
 
 
